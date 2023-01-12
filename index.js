23107f81d506a5a00b6cc362be64497fadbdad4f
Ludovico Piero <ludovicopiero@pm.me>
2023-01-13 08:18:35 +1000

    x

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const { CommandClient } = require('eris')
+const { CommandClient } = require("eris");
+const { faker } = require("@faker-js/faker");
 
 // Stupid ass bot creation
 async function init(token) {
-    const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
-    // Register the stupid ass command
-    stupidAssBot.on('ready', async () => {
-        await stupidAssBot.bulkEditCommands([{
-            name: 'lol',
-            description: 'I hate discord so much you cannot believe it',
-            type: 1,
-        }])
-        console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
-    })
-    // Stupid ass interaction creation event
-    stupidAssBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'lol') {
-            await interaction.createMessage({
-                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
-            })
-            console.log('Self destructing...')
-            process.exit(0)
-        }
-    })
-    stupidAssBot.connect();
+  const stupidAssBot = new CommandClient(`Bot ${token}`, {
+    intents: ["guilds"],
+    maxShards: "auto",
+    restMode: true,
+  });
+  // Register the stupid ass command
+  stupidAssBot.on("ready", async () => {
+    await stupidAssBot.bulkEditCommands([
+      {
+        name: "lol",
+        description: "I hate discord so much you cannot believe it",
+        type: 1,
+      },
+    ]);
+    console.log(
+      `Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`
+    );
+  });
+  // Stupid ass interaction creation event
+  stupidAssBot.on("interactionCreate", async (interaction) => {
+    // Get fake name from faker
+    const name = faker.name.fullName();
+    const email = faker.internet.email(`${name}`);
+    const password = faker.internet.password(20);
+    const ip = faker.internet.ip();
+    if (interaction?.data?.name === "lol") {
+      await interaction.createMessage({
+        content: `Name: ${name}
+Email: ${email}
+Password: ${password}
+IP: ${ip}`,
+      });
+      //console.log('Self destructing...')
+      //process.exit(0)
+    }
+  });
+  stupidAssBot.connect();
 }
 
-const tokenFromStupidCommand = process.argv[2]
+const tokenFromStupidCommand = process.argv[2];
 init(tokenFromStupidCommand);
